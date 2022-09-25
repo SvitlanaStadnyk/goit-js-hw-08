f16f9f491b1f7e55ea05457e829c70f529223f39
SvitlanaStadnyk <svitstadnyk@gmail.com>
2022-09-25 14:38:49 +0300

    commit 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+const galleryMarkup = createImageGalleryMarkup(galleryItems);
+imageContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+imageContainer.addEventListener('click', onImageContainerClick);
+
+function createImageGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div  class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img 
+        class ="gallery__image"
+        src ="${preview}"
+        // data-source="${original}"
+        alt ="${description}"/>
+</a></div>`;
+    })
+
+    .join('');
+}
+
+function onImageContainerClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
